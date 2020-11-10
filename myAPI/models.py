fc6f11d2661c250febb0d6ef399229a2f64087ff
Muhammad Yasir Abdulazis <rayas143120@gmail.com>
2020-11-10 20:32:02 +0700

    update table

--- myAPI/models.py
+++ myAPI/models.py
@@ -12,4 +12,19 @@ class Barang(models.Model):
         return self.name_barang
 
     
-# class Penjualan(models.Model):
+class Penjualan(models.Model):
+    total_pembelian = models.FloatField()
+    tanggal_penjualan = models.DateTimeField(auto_now_add=True)
+    keterangan_penjualan = models.TextField()
+
+    def __str__(self):
+        return self.total_pembelian
+
+
+class Detail_Penjualan(models.Model):
+    penjualan_id = models.ForeignKey(Penjualan, on_delete=models.CASCADE)
+    barang_id = models.ForeignKey(Barang, on_delete=models.CASCADE)
+    qty = models.IntegerField()
+
+    def __str__(self):
+        return self.penjualan_id
